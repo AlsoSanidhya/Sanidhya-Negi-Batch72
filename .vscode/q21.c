47439730fa59c1585efd2bf054133fa1dd3f5be1
AlsoSanidhya <waynesfr@gmail.com>
2025-10-14 11:37:12 +0530

    max and min with arrays and fuctions

---
+++ .vscode/q21.c
@@ -0,0 +1,43 @@
+//Write a C program to find the maximum and minimum elements in an array using functions.
+#include <stdio.h>
+// Function to find maximum element
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+// Function to find minimum element
+int findMin(int arr[], int n) {
+    int min = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < min)
+            min = arr[i];
+    }
+    return min;
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = findMax(arr, n);
+    int min = findMin(arr, n);
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
